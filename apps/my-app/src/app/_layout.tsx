43a67048f190c3593932c93a1e8578bb20d66468
Aman Varshney <amanvarshney.work@gmail.com>
2025-11-08 23:15:24 +0530

    ok

--- apps/my-app/src/app/_layout.tsx
+++ apps/my-app/src/app/_layout.tsx
@@ -1,7 +1,15 @@
 import "../global.css";
-
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Slot } from "expo-router";
 
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL || "", {
+  unsavedChangesWarning: false,
+});
+
 export default function Layout() {
-  return <Slot />;
+  return (
+    <ConvexProvider client={convex}>
+      <Slot />
+    </ConvexProvider >
+  )
 }
